4f039d5868d435b64fbb0d7925f207be080c3c7f
John Persson <john.richard.persson@gmail.com>
2024-12-06 17:32:05 +0100

    part 1

--- 2024/day04.py
+++ 2024/day04.py
@@ -6,16 +6,30 @@ with open('day04.txt', 'r') as file:
 
     pattern = r"XMAS"
     total = 0
-    def countHorisontalXmas(lines):
-        found = 0
-        for line in lines:
-            matches = re.findall(pattern,line)
-            found += len(matches)
-            matches = re.findall(pattern,line [::-1])
-            found += len(matches)
+    def countXmas(line):
+        found = 0   
+        matches = re.findall(pattern,line)
+        found += len(matches)
+        matches = re.findall(pattern,line [::-1])
+        found += len(matches)
         return found
     matrix = list()
     for i in range(len(lines)):
         matrix.append(list(lines[i]))
     print(matrix)
-    total += countHorisontalXmas(lines)
+
+    def checkMatrix(matrix):
+        total = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i +3 < len(matrix):
+                    total += countXmas(''.join([matrix[i][j],matrix[i+1][j],matrix[i+2][j],matrix[i+3][j]]))
+                if j + 3 < len(matrix[i]):
+                    total += countXmas(''.join([matrix[i][j],matrix[i][j+1],matrix[i][j+2],matrix[i][j+3]]))
+                if i +3 < len(matrix) and j + 3 < len(matrix[i]):
+                    total += countXmas(''.join([matrix[i][j],matrix[i+1][j+1],matrix[i+2][j+2],matrix[i+3][j+3]]))
+                    total += countXmas(''.join([matrix[i][j+3],matrix[i+1][j+2],matrix[i+2][j+1],matrix[i+3][j]]))
+        
+        print(total)
+    # total += countHorisontalXmas(lines)
+    checkMatrix(matrix)
